a35ddfb123812c58e6fc095ce066c0db1b4e4c6d
Sergey Vatagin <s.vatagin@gmail.com>
2016-03-29 14:07:02 +0300

    HOTFIX: DEL apostrophs in currentWeather

--- app/components/weather/current/CurrentWeather.js
+++ app/components/weather/current/CurrentWeather.js
@@ -17,15 +17,15 @@ export default class CurrentWeather extends Component {
     const tableRows = [
       [
         <span key={'temp1'}>Temperature: <strong key>{temp} {String.fromCharCode(0x00B0)}C</strong></span>,
-          <span key={'desc1'}>'Description: ', <strong>{weatherDesc.description}</strong></span>,
+          <span key={'desc1'}>Description: <strong>{weatherDesc.description}</strong></span>,
       ],
       [
-        <span key={'hum1'}>'Humidity: ', <strong>{humidity} %</strong></span>,
-          <span key={'deg1'}>Wind degree: ', <strong>{Math.round(wind.deg)} {String.fromCharCode(0x00B0)}</strong></span>
+        <span key={'hum1'}>'Humidity: <strong>{humidity} %</strong></span>,
+          <span key={'deg1'}>Wind degree: <strong>{Math.round(wind.deg)} {String.fromCharCode(0x00B0)}</strong></span>
       ],
       [
-        <span key={'pres1'}>Pressure: ', <strong>{ Math.round(pressure * hpaToAtm) } mm Hg</strong></span>,
-        <span key={'speed1'}>Wind speed: ', <strong>{wind.speed} m/sec</strong></span>
+        <span key={'pres1'}>Pressure: <strong>{ Math.round(pressure * hpaToAtm) } mm Hg</strong></span>,
+        <span key={'speed1'}>Wind speed: <strong>{wind.speed} m/sec</strong></span>
       ]
     ];
     const tBody = tableRows.map( (row, index) => {
